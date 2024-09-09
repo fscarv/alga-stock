5b6f9748c92e817694612b42ed1def89ef67330c
fscarv <fernando.carvalho@lcsistemas.com.br>
2024-09-09 10:55:44 -0300

    exemplo de ciclo de vida ods componentes

--- src/componentes/ClassComponente/ClassComponente.tsx
+++ src/componentes/ClassComponente/ClassComponente.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 
 class ClassComponente extends React.Component<{ name: string }> {
+    constructor(props: any){
+        super(props)
+        console.log('constructor reached')
+    }
+
     state = {
         name: 'Mundo!'
     }
 
+    componentDidMount() {
+        console.log('componentDidMount reached')
+    }
+
+    componentDidUpdate() {
+        console.log('componentDidUpdate reached')
+    }
+
     render() {
+        console.log('render reached')
         return <div>
             <p>name: {this.state.name}</p>
             <button onClick={() =>{

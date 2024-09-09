cbb0a16986c62a97132506d5a6646dc8c0126e5e
fscarv <fernando.carvalho@lcsistemas.com.br>
2024-09-09 09:23:53 -0300

    criado TesteComponente e movido App para o diretorio de componentes

---
+++ src/componentes/TesteComponente/TesteComponente.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './TesteComponente.css'
+
+const TesteComponente = () => (
+<div className='TesteComponente'>Teste Componente</div>
+)
+
+export default TesteComponente

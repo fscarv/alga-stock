121d6ae5e95237aa5fb03062ca2758a5e87d949a
fscarv <fernando.carvalho@lcsistemas.com.br>
2024-09-09 15:50:07 -0300

    adicionado componente de botão

--- src/componentes/App/App.tsx
+++ src/componentes/App/App.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.css';
 import Header from '../Header';
+import Button from '../Button';
+
+function TestComponent (){
+  return <img width="16" src="https://cdn1.iconfinder.com/data/icons/search-43/512/20_lense_search_tool_scan-512.png" alt='Lupa' />
+}
 
 function App() {
   return (
     <div className="App">
-      <Header title={'AlgaStock'} />
-      App
+      <Header title="AlgaStock" />
+      
+      <div className='Container'>
+        <Button 
+          onClick={() => window.alert('Clicked!')} 
+          appendIcon={<TestComponent />}
+        >
+         Alert 
+        </Button>
+      </div>
     </div>
   );
 }

d751961589a1d96d2948f67529a8adcbe969af6a
fscarv <fernando.carvalho@lcsistemas.com.br>
2024-09-09 17:00:52 -0300

    adicionado componente input

--- src/componentes/App/App.tsx
+++ src/componentes/App/App.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from '../Header';
 import Button from '../../shared/Button';
 import Container from '../../shared/Container';
+import Input from '../../shared/Input';
 
 function TestComponent (){
   return <img width="16" src="https://cdn1.iconfinder.com/data/icons/search-43/512/20_lense_search_tool_scan-512.png" alt='Lupa' />
 }
 
 function App() {
+  const [street, setStreet] = useState('')
   return (
     <div className="App">
       <Header title="AlgaStock" />
@@ -20,6 +22,12 @@ function App() {
         >
          Alert 
         </Button>
+        <Input 
+          label="Street" 
+          placeholder='E.g.: 15h Avenue'
+          value={street}
+          onChange={(e) => setStreet(e.target.value)}
+        />
       </Container>
     </div>
   );

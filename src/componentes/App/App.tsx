ad0b831eb14ae21111fcfd1213fd2161fd689434
fscarv <fernando.carvalho@lcsistemas.com.br>
2024-09-10 15:16:24 -0300

    adicionado o componente Form

--- src/componentes/App/App.tsx
+++ src/componentes/App/App.tsx
@@ -4,6 +4,9 @@ import Header from '../Header';
 import Container from '../../shared/Container';
 import Table, { TableHeader } from '../../shared/Table';
 import Products from '../../shared/Table/Table.mockdata';
+import Form from '../../shared/Form';
+import Input from '../../shared/Input';
+import Button from '../../shared/Button';
 
 const headers: TableHeader[] = [
   {key: 'id', value: '#'},
@@ -21,6 +24,28 @@ function App() {
           headers={headers}
           data={Products}
         />
+        <Form title='Product Form' onSubmit={console.log}>
+          <Input 
+            label='Name'
+            placeholder='Ex. Product name'
+          />
+          <Input 
+            label='Price'
+            type='number'
+            step='0.01'
+            min='0'
+            placeholder='Ex. 1.99'
+          />
+          <Input 
+            label='Stock'
+            type='number'
+            min='0'
+            placeholder='Ex. 10'
+          />
+          <Button>
+            Submit
+          </Button>
+        </Form>
       </Container>
     </div>
   );

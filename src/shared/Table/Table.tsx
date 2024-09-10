632d8f8289cf12be4c40c96c25fb311b4d5cfbc0
fscarv <fernando.carvalho@lcsistemas.com.br>
2024-09-10 09:31:18 -0300

    adicionado o pacote sass e estilizacao da tabela

--- src/shared/Table/Table.tsx
+++ src/shared/Table/Table.tsx
@@ -1,30 +1,30 @@
 import React from "react";
-//import './Table.scss';
+import './Table.scss';
 
 const Table = () => {
-    return <table>
+    return <table className="AppTable">
         <thead>
             <tr>
                 <th>Product</th>
                 <th>Price</th>
-                <th>Stock</th>
+                <th className="right">Stock</th>
             </tr>
         </thead>
         <tbody>
             <tr>
                 <td>Cookie</td>
                 <td>$0.99</td>
-                <td>50</td>
+                <td className="right">50</td>
             </tr>
             <tr>
                 <td>Milk 1L</td>
                 <td>$1.99</td>
-                <td>20</td>
+                <td className="right">20</td>
             </tr>
             <tr>
                 <td>Bread</td>
                 <td>$0.99</td>
-                <td>100</td>
+                <td className="right">100</td>
             </tr>
         </tbody>
     </table>

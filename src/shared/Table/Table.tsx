a02a657c61389832178464f9151c73d0d74df08d
fscarv <fernando.carvalho@lcsistemas.com.br>
2024-09-10 08:48:28 -0300

    adicionado estrutura html de tabelas

--- src/shared/Table/Table.tsx
+++ src/shared/Table/Table.tsx
@@ -1,9 +1,32 @@
 import React from "react";
-import './Table.scss';
+//import './Table.scss';
 
 const Table = () => {
     return <table>
-        
+        <thead>
+            <tr>
+                <th>Product</th>
+                <th>Price</th>
+                <th>Stock</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Cookie</td>
+                <td>$0.99</td>
+                <td>50</td>
+            </tr>
+            <tr>
+                <td>Milk 1L</td>
+                <td>$1.99</td>
+                <td>20</td>
+            </tr>
+            <tr>
+                <td>Bread</td>
+                <td>$0.99</td>
+                <td>100</td>
+            </tr>
+        </tbody>
     </table>
 }
 

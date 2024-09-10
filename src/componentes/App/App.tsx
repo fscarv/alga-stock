48a71093c8160216393c17762a31ab9359484634
fscarv <fernando.carvalho@lcsistemas.com.br>
2024-09-10 08:34:47 -0300

    limpeza app e adicionado componente base de tabelas

--- src/componentes/App/App.tsx
+++ src/componentes/App/App.tsx
@@ -1,30 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import Header from '../Header';
-import Button from '../../shared/Button';
 import Container from '../../shared/Container';
-import Input from '../../shared/Input';
 
-function TestComponent (){
-  return <img width="16" src="https://cdn1.iconfinder.com/data/icons/search-43/512/20_lense_search_tool_scan-512.png" alt='Lupa' />
-}
-
-function App() {
-  const [street, setStreet] = useState('')
+function App() { 
   return (
     <div className="App">
-      <Header title="AlgaStock" />
-      
+      <Header title="AlgaStock" />     
       <Container>
-        <ul>
-        {
-        ['Abacate', 'Melancia', 'Manga', 'Abacate'].map((fruit, index) => {
-          return <li key={index}>
-            {fruit}
-          </li>
-        })
-        }
-        </ul>
+        
       </Container>
     </div>
   );

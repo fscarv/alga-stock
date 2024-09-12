986f22192301b623a461de5a8b3aa128cc9f4917
fscarv <fernando.carvalho@lcsistemas.com.br>
2024-09-12 14:57:56 -0300

    adicionado mock server

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const http = axios.create({
-    baseURL: 'http://jsonplaceholder.typicode.com'
+    baseURL: 'http://127.0.0.1:4010/',
+    headers: {
+        authorization: 'Bearer 123'
+    }
 })
 
 export default http
